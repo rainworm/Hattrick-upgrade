5dbf65ed208ffee4ad5a3cd32f39e53195f594c7
Matjaz <matjaz.senicar@gmail.com>
2011-11-09 10:56:40 +0100

    changed the way we get avatars and added Team of the week avatars.

--- hattrick_players_avatars.user.js
+++ hattrick_players_avatars.user.js
@@ -2,36 +2,16 @@
 // @name           HattrickPlayersAvatars
 // @namespace      HattrickPlayersAvatars
 // @include        http://www*.hattrick.org/Club/Players/*
+// @include        http://www*.hattrick.org/World/Series/TeamOfTheWeek.aspx?leagueLevelUnitId=*&teamId=*
 // ==/UserScript==
 
 allDivisions = document.getElementsByTagName("div");
+allAs = document.getElementsByTagName('a');
 Player = new Array;
 
 //Function fills array with player data
-function f_gatherPlayerData() {
-  counter = 0;
-
-  //the method of getting players name on individual player page
-  if ((document.location.href.toLowerCase().indexOf('playerid=') != -1)) {
-    allH1 = document.getElementsByTagName('h1');
-    for (i=0; i<allH1.length; i++) {
-      PlayerName = allH1[i].innerHTML.replace(/^\s+|\s+$/g, '');
-      PlayerName = PlayerName.substr(PlayerName.indexOf('</span>')+7, PlayerName.length);
-      PlayerName = PlayerName.replace(/^\s+|\s+$/g, '');
-      PlayerName = PlayerName.substr(0, PlayerName.indexOf(' <span>'));
-    }
-  }
-
-  //We walk trough all divisions
-  for (i=0; i < allDivisions.length; i++) {
-    //and get player data for each player (we'll needed it to uniqly define Players look)
-    if (allDivisions[i].className.match("playerInfo")) {
+function f_gatherPlayerData(PlayerName, counter) {
       Player[counter] = new Array;
-
-      if ((document.location.href.toLowerCase().indexOf('teamid=') != -1)) {
-        PlayerName = allDivisions[i].innerHTML.substring(allDivisions[i].innerHTML.indexOf('title=')+7, allDivisions[i].innerHTML.indexOf('title=')+40);
-        PlayerName = PlayerName.substring(0, PlayerName.indexOf('">'));
-      }
 
       PlayerName = PlayerName.replace('  ', ' ');
 
@@ -115,27 +95,26 @@ function f_gatherPlayerData() {
       }
 
       Player[counter][13] = HairColor;
+}
 
-      counter++;
-    }
-  }
+function f_draw_avatar(counter) {
+  return '<img alt="" style="left:9px;top:10px;" src="/Img/Avatar/bodies/f'+Player[counter][1]+'man'+Player[counter][2]+Player[counter][3]+'.png">' +
+         '<img alt="" style="left:9px;top:10px;" src="/Img/Avatar/faces/f'+Player[counter][4]+Player[counter][5]+'.png">' +
+         '<img alt="" style="left:24px;top:8px;" src="/Img/Avatar/eyes/e'+Player[counter][6]+Player[counter][7]+'.png">' +
+         '<img alt="" style="left:31px;top:32px;" src="/Img/Avatar/mouths/m'+Player[counter][8]+Player[counter][9]+'.png">' +
+         '<img alt="" style="left:19px;top:8px;" src="/Img/Avatar/noses/n'+Player[counter][10]+'.png">' +
+         '<img alt="" style="left:9px;top:10px;" src="/Img/Avatar/hair/f'+Player[counter][11]+'h'+Player[counter][12]+Player[counter][13]+'.png">';
 }
 
 //Function sets an avatar for each player
-function f_set_avatar() {
+function f_set_new_avatar() {
   counter = 0;
   //We walk trough all divisions
   for (i=0; i < allDivisions.length; i++) {
     //and we modify the ones that are ment for displaying silhuets/avatars
     if (allDivisions[i].className.match("faceCard")) {
       allDivisions[i].style.backgroundImage = "url(/Img/Avatar/backgrounds/card1.png)";
-      allDivisions[i].innerHTML = '<img alt="" style="left:9px;top:10px;" src="/Img/Avatar/bodies/f'+Player[counter][1]+'man'+Player[counter][2]+Player[counter][3]+'.png">' +
-                                  '<img alt="" style="left:9px;top:10px;" src="/Img/Avatar/faces/f'+Player[counter][4]+Player[counter][5]+'.png">' +
-                                  '<img alt="" style="left:24px;top:8px;" src="/Img/Avatar/eyes/e'+Player[counter][6]+Player[counter][7]+'.png">' +
-                                  '<img alt="" style="left:31px;top:32px;" src="/Img/Avatar/mouths/m'+Player[counter][8]+Player[counter][9]+'.png">' +
-                                  '<img alt="" style="left:19px;top:8px;" src="/Img/Avatar/noses/n'+Player[counter][10]+'.png">' +
-                                  '<img alt="" style="left:9px;top:10px;" src="/Img/Avatar/hair/f'+Player[counter][11]+'h'+Player[counter][12]+Player[counter][13]+'.png">' +
-                                  allDivisions[i].innerHTML;
+      allDivisions[i].innerHTML = f_draw_avatar(counter) + allDivisions[i].innerHTML;
       counter++;
     }
   }
@@ -143,8 +122,44 @@ function f_set_avatar() {
 
 //initialization
 function f_init() {
-  f_gatherPlayerData();
-  f_set_avatar();
+  //f_gatherPlayerData();
+  counter = 0;
+
+  //the method of getting players name on individual player page
+  if ((document.location.href.toLowerCase().indexOf('playerid=') != -1)) {
+    allH1 = document.getElementsByTagName('h1');
+    for (i=0; i<allH1.length; i++) {
+      PlayerName = allH1[i].innerHTML.replace(/^\s+|\s+$/g, '');
+      PlayerName = PlayerName.substr(PlayerName.indexOf('</span>')+7, PlayerName.length);
+      PlayerName = PlayerName.replace(/^\s+|\s+$/g, '');
+      PlayerName = PlayerName.substr(0, PlayerName.indexOf(' <span>'));
+    }
+  }
+
+  //we go trough all links
+  for (i=0; i < allAs.length; i++) {
+    //that refer to player and gather player data
+    if (allAs[i].href.indexOf('/Club/Players/Player.aspx?playerId=') != -1) {
+      f_gatherPlayerData(allAs[i].innerHTML, counter);
+      counter++;
+    }
+  }
+
+  counter = 0;
+  //we walk trough all divisions
+  for (i=0; i < allDivisions.length; i++) {
+    //and get player data for each player (we'll needed it to uniqly define Players look)
+    if (allDivisions[i].className.match("playerInfo")) {
+      if ((document.location.href.toLowerCase().indexOf('teamid=') != -1)) {
+        PlayerName = allDivisions[i].innerHTML.substring(allDivisions[i].innerHTML.indexOf('title=')+7, allDivisions[i].innerHTML.indexOf('title=')+40);
+        PlayerName = PlayerName.substring(0, PlayerName.indexOf('">'));
+      }
+      f_gatherPlayerData(PlayerName, counter);
+      counter++;
+    }
+  }
+
+  f_set_new_avatar();
 }
 
 f_init();
